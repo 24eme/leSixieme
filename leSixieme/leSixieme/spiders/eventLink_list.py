597f8df6709559e81c6690db8be9491d8846ec2d
Honorine Marie Christy <honorine@24eme.fr>
2020-07-21 16:03:44 +0200

    récupère les urls des liens et les écrit dans un json

--- leSixieme/leSixieme/spiders/eventLink_list.py
+++ leSixieme/leSixieme/spiders/eventLink_list.py
@@ -1,28 +1,39 @@
 import scrapy
 import json
+import os
 
-filename = 'export.json'
+filename = 'eventsLinks.json'
 
 class IntroSpider(scrapy.Spider):
     name = "eventLink"
 
     def start_requests(self):
-        url='https://www.eventbrite.fr'
+        url='http://eventbrite.fr/d/france--paris/paris//'
         yield scrapy.Request(url=url, callback=self.parse)
 
-#eds-event-card-content__action-link class pour les liens
 
     def parse(self,response):
         list_data=[]
-        eventLink_list = response.css('eds-event-card-content__action-link > h3 > a::attr(title)').extract()
+        eventLink_list = response.xpath('.//div[@class="eds-event-card-content__content-container eds-l-pad-right-2"]/div[@class="eds-event-card-content__content"]/div[@class="eds-event-card-content__content__principal"]/div[@class="eds-event-card-content__primary-content"]/a/@href').extract()
 
+        i=0;
         for eventLink in eventLink_list:
-            data=[eventLink_list[i]]
-            i=i+1
+            data={
+                'url' : eventLink_list[i]
+            }
+            i+=1
             list_data.append(data)
 
-        # print(list_data)
+
+        # supprime le json s'il existe déjà
+
+        if os.path.exists("eventsLinks.json"):
+            os.remove("eventsLinks.json")
+
         with open(filename, 'a+') as f:   # Writing data in the file
             for data in list_data :
                 app_json = json.dumps(data)
                 f.write(app_json+"\n")
+
+
+#pour lancer dans le terminal :  cd leSixieme/leSixieme$ scrapy crawl eventLink
