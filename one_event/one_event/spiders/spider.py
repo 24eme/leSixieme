893b4b9e6904d05319aadc5eae205c531b2bdda4
rilerss <rilerss.ravendar@gmail.com>
2020-07-22 10:58:15 +0200

    modification récup description/adresse

--- one_event/one_event/spiders/spider.py
+++ one_event/one_event/spiders/spider.py
@@ -3,8 +3,8 @@ import scrapy
 
 class SpiderSpider(scrapy.Spider):
     name = 'one_event'
-    allowed_domains = ['eventbrite.fr/e/billets-coding-cake-paris-86781705417?aff=ebdssbdestsearch']
-    start_urls = ['http://eventbrite.fr/e/billets-coding-cake-paris-86781705417?aff=ebdssbdestsearch/']
+    allowed_domains = ['eventbrite.co.uk/e/fashion-masterclass-paris-tickets-110725982394?aff=ebdssbdestsearch']
+    start_urls = ['https://www.eventbrite.co.uk/e/fashion-masterclass-paris-tickets-110725982394?aff=ebdssbdestsearch']
 
     def parse(self, response):
         main=response.xpath('//div[@class="event-listing__body l-sm-pad-top-0"]')
@@ -14,15 +14,16 @@ class SpiderSpider(scrapy.Spider):
         date=main.xpath('.//p[@class="js-date-time-first-line"]/text()').extract_first()
         heure=main.xpath('.//p[@class="js-date-time-second-line"]/text()').extract_first()
 
+        #description=main.css('.structured-content').extract()
         all_para=main.xpath('.//div[@class="structured-content-rich-text structured-content__module l-align-left l-mar-vert-6 l-sm-mar-vert-4 text-body-medium"]/p')
-        description=''
+        description=[]
         
         for para in all_para:
-            cont= para.xpath('.//text()').extract_first();
-            description=description+'\n'+cont
+            cont= para.xpath('.//text()').extract();
+            description.append(cont)
         
         prix=main.xpath('.//div[@class="js-display-price"]/text()').extract_first()
-        adresse=main.xpath('.//div[@class="event-details__data"]/text()').extract_first()
+        adresse=main.xpath('.//div[@class="event-details__data"]/p/text()').extract()
         
         print(titre)
         print(image_url)
@@ -31,3 +32,4 @@ class SpiderSpider(scrapy.Spider):
         print(description)
         print(prix)
         print(adresse)
+        
