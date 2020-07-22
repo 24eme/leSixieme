e628c1639b3deda48b2f16f71d3d6de92571e1aa
Sidi Sokhona <ssokhona@24eme.fr>
2020-07-22 12:02:06 +0200

    formatage de l'heure en enlèvantle CEST

--- one_event/one_event/spiders/spider.py
+++ one_event/one_event/spiders/spider.py
@@ -3,20 +3,20 @@ import scrapy
 
 class SpiderSpider(scrapy.Spider):
     name = 'one_event'
-    
+
     def start_requests(self):
         url = 'https://www.eventbrite.co.uk/e/fashion-masterclass-paris-tickets-110725982394?aff=ebdssbdestsearch'
         yield scrapy.Request(url=url, callback=self.parse)
-    
+
 
     def parse(self, response):
         main=response.xpath('//div[@class="event-listing__body l-sm-pad-top-0"]')
-        
+
         titre=main.xpath('.//h1[@class="listing-hero-title"]/text()').extract_first()
         image_url=main.xpath('.//picture/@content').extract_first()
         date=main.xpath('.//p[@class="js-date-time-first-line"]/text()').extract_first()
         heure=main.xpath('.//p[@class="js-date-time-second-line"]/text()').extract_first()
-        
+
         def descrip(select):
             description=[]
             for data in select:
@@ -25,16 +25,16 @@ class SpiderSpider(scrapy.Spider):
             return(description)
 
         all_para=main.xpath('.//div[@class="structured-content-rich-text structured-content__module l-align-left l-mar-vert-6 l-sm-mar-vert-4 text-body-medium"]/p')
-        
+
         description=(descrip(all_para))
-        
+
         prix=main.xpath('.//div[@class="js-display-price"]/text()').extract_first()
         adresse=main.xpath('.//div[@class="event-details__data"]/p/text()').extract()
-        
+
         print(titre)
         print(image_url)
         print(date)
-        print(heure)
+        print(heure.replace("CEST",""))
         print(description)
         print(prix)
         print(adresse)
