9c56f79cf056d3023b5bf82c18c8e002c09ff4a1
Honorine Marie Christy <honorine@24eme.fr>
2020-07-28 12:28:02 +0200

    changement du header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -4,7 +4,7 @@
   <ul class="">
     <li class="nav-item"><label for="">Nom :</label><input type="text" name="name_search" value="Recherche par nom"></li>
     <li class="nav-item"><label for="">Secteur :</label><select class="" name=""><option id="culture" value="Culture">Culture</option><option id="fete" value="Informatique">Fete</option></select></li>
-    <li class="nav-item"><label for="">Arrondisement :</label><input onkeydown="search_arrondissement()"type="text" id="arrondisement" placeholder="09"></li>
+    <li class="nav-item"><label for="">Arrondisement :</label><input type="text" id="arrondissement" placeholder="09"></li>
   </ul>
 </nav>
 <div class="btn-group">
