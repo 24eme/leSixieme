4c6955483a8ee1564992b12af49bd3fa5595e7d5
Honorine Marie Christy <honorine@24eme.fr>
2020-08-05 17:49:36 +0200

    appel d'un seul script car erreur dans le script.js

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -2,8 +2,6 @@
 <script src="{{ asset('leaflet-routing-machine-3.2.12/dist/leaflet-routing-machine.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
-
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
@@ -16,9 +14,9 @@
 
 <script src="https://cdn.jsdelivr.net/npm/places.js@1.19.0"></script>
 
-<script src="{{ asset('js/script.js') }}" ></script>
- <script src="{{ asset('js/map_src.js') }}" ></script>
-=======
+<!-- <script src="{{ asset('js/script.js') }}" ></script>
+ <script src="{{ asset('js/map_src.js') }}" ></script> -->
+
 <!-- <script type="text/json" src="{{ asset('js/scops.json') }}" ></script> -->
 <!-- <script src="{{ asset('js/map_init.js') }}" ></script> -->
 <!-- <script src="{{ asset('js/map.js') }}" ></script> -->
@@ -32,4 +30,3 @@
 <!-- hono test-->
 <script src="{{ asset('js/filters.js') }}" ></script>
 <!-- fin hono test -->
->>>>>>> f933ba6e12ad9ec08e93d3b9a3b33efb70e99f42
