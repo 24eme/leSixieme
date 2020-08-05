1cd697457008a6054ffd088a4f197ffb40f0f9b0
Honorine Marie Christy <honorine@24eme.fr>
2020-08-05 11:32:31 +0200

    appel du script filters

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -3,11 +3,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <!-- <script type="text/json" src="{{ asset('js/scops.json') }}" ></script> -->
-<script src="{{ asset('js/map_init.js') }}" ></script>
+<!-- <script src="{{ asset('js/map_init.js') }}" ></script> -->
 <!-- <script src="{{ asset('js/map.js') }}" ></script> -->
-<script src="{{ asset('js/events_list.js') }}" ></script>
+<!-- <script src="{{ asset('js/events_list.js') }}" ></script>
 <script src="{{ asset('js/filter_map.js') }}" ></script>
-<script src="{{ asset('js/script.js') }}" ></script>
+<script src="{{ asset('js/script.js') }}" ></script> -->
 <!-- <script src="{{ asset('js/map_src.js') }}" ></script>
 <script src="{{ asset('js/map_src.js') }}" ></script> -->
 <!-- <script type="text/javascript" src="js/script.js"></script> -->
+
+<!-- hono test-->
+<script src="{{ asset('js/filters.js') }}" ></script>
+<!-- fin hono test -->
