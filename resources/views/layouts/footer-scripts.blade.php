b6863b1bf9a335006134e62dc22b0b6d15bb85a6
Honorine Marie Christy <honorine@24eme.fr>
2020-08-06 15:05:29 +0200

    appel de 2 scripts seulement filters et list

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,10 +1,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  
-<script src="{{asset('js/script.js')}}"></script>
-<script src="{{asset('js/map_src.js')}}"></script>
+
+<!-- <script src="{{asset('js/script.js')}}"></script>
+<script src="{{asset('js/map_src.js')}}"></script> -->
 
 <!-- hono test-->
-<!-- <script src="{{ asset('js/filters.js') }}" ></script> -->
+<script src="{{ asset('js/filters.js') }}" ></script>
+<script src="{{ asset('js/list.js') }}" ></script>
+
 <!-- fin hono test -->
