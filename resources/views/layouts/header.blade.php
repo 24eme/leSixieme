95f34b78b4bf9f2463459e138b459f1add1b5dca
Honorine Marie Christy <honorine@24eme.fr>
2020-08-06 17:00:08 +0200

    ajout des km dans le header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,6 +1,21 @@
 <header><nav >
   <ul>
     <li>
+
+            <label> Filtres:</label>
+            <button id='filter-icon' class='btn-primary' type='button' name='button' onclick="openFilters()"><i class='fas fa-filter'></i></button>
+            <button id='reinitialiser' class='btn-primary' type='button' name='re-button'><i class="fas fa-redo-alt"></i></button>
+            <label>Km</label>
+            <select name="km" id="km">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+            </select>
+
+    </li>
       <!-- <label>Adresse :</label><input type="text" name="adresse_search"value="votre adresse"> -->
       <!-- <div class="dropdown">
   <li id="title_secteur" class="btn dropdown-toggle" data-toggle="dropdown">Par secteur :</li>
@@ -31,21 +46,8 @@
                     <option value="5">5</option>
                     <option value="6">6</option>
                   </select></li> -->
-                      <!-- <label>Km</label>
-                      <select name="km" id="km">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                      </select> -->
 
-                      <label> Filtres:</label>
-                      <button id='filter-icon' class='btn-primary' type='button' name='button' onclick="openFilters()"><i class='fas fa-filter'></i></button>
-      <button id='reinitialiser' class='btn-primary' type='button' name='re-button'><i class="fas fa-redo-alt"></i></button>
-                      </li>
-                      <nav id='open-filters' style="display:none">
+        <nav id='open-filters' style="display:none">
          <hr>
          <li>
            <label>Date Debut:</label> <input type="date" id="dateDeb"value="date de début">
@@ -61,39 +63,37 @@
                  <option value="Loisirs">Loisirs</option>
                  <option value="Festival">Festival</option>
                  <option value="Culturel">Culturel</option>
-                 <!-- <option value="4">4</option>
-  <option value="5">5</option>
-  <option value="6">6</option> -->
-</select>
-<label>Arrondissement:</label>
-<select name="arrondissement" id="arrondissement">
-<option value="Tous">Tous</option>
-<option value="01">01</option>
-<option value="02">02</option>
-<option value="03">03</option>
-<option value="04">04</option>
-<option value="05">05</option>
-<option value="06">06</option>
-<option value="07">07</option>
-<option value="08">08</option>
-<option value="09">09</option>
-<option value="10">10</option>
-<option value="11">11</option>
-<option value="12">12</option>
-<option value="13">13</option>
-<option value="14">14</option>
-<option value="15">15</option>
-<option value="16">16</option>
-<option value="17">17</option>
-<option value="18">18</option>
-<option value="19">19</option>
-<option value="20">20</option>
-</select>
-</li>
-<button id='validate-filter' class='btn-primary' type='button' name='button' onclick="filter();filter_list()"> Filtrer</button>
+
+          </select>
+          <label>Arrondissement:</label>
+          <select name="arrondissement" id="arrondissement">
+                <option value="Tous">Tous</option>
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+                <option value="18">18</option>
+                <option value="19">19</option>
+                <option value="20">20</option>
+            </select>
+        </li>
+        <button id='validate-filter' class='btn-primary' type='button' name='button' onclick="filter();filter_list()"> Filtrer</button>
 
 
-</nav>
+        </nav>
 </nav>
 
 
