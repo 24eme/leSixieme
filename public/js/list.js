a8fe211f82ee0af94434f32c4596b9ada3ece0d7
rilerss <rilerss.ravendar@gmail.com>
2020-08-06 16:14:42 +0200

    add refresh list

--- public/js/list.js
+++ public/js/list.js
@@ -121,6 +121,11 @@ function filter_list(){
 	updateList();
 }
 
+document.getElementById("reinitialiser").addEventListener('click',function(event) {
+	document.getElementById('ct').innerHTML="";
+  	initList();
+});
+
 $(document).ready(function(){
 	initList();
 });
