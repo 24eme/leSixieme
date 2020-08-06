7cee3942131c888cfc75f92228d7b0ea56460bd5
Honorine Marie Christy <honorine@24eme.fr>
2020-08-06 15:04:56 +0200

    appel de la bonne liste

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout')
 @section('content')
-<ul  class="nav_events" id="nav_events"></ul>
 <!-- <button type="button" id="others" class="btn btn-secondary btn_searchZone">Recherche dans cette zone</button> -->
 <div id="map"></div>
+<ul  class="nav_events" id="ct"></ul>
+
 @endsection
