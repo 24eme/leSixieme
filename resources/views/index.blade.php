b11461af5a2695657ea0ffdf10fbb19ff1d09409
Sidi Sokhona <ssokhona@24eme.fr>
2020-08-10 17:05:18 +0200

    version ug mis en commentaire avec bootstrap

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,5 +3,7 @@
 
 <!-- <button type="button" id="others" class="btn btn-secondary btn_searchZone">Recherche dans cette zone</button> -->
 <div class=" col-sm-6 col-xl-8 col-lg-7 col-md-6 m-auto" id="map"></div>
-<div class='list'><ul  class="nav_events col-lg-4 col-sm-6 col-md-4" id="ct"></ul></div>
+<div id='list'><ul  class="nav_events  " id="ct"></ul></div>
+<!-- Sidi -->
+<!-- <div id='list'><ul  class="nav_events col-lg-4 col-sm-6 col-md" id="ct"></ul></div> -->
 @endsection
