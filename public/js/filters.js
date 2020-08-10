c5e8e55049f0b827a595fe9b1eb1aa5860f13150
Honorine Marie Christy <honorine@24eme.fr>
2020-08-10 12:29:47 +0200

    suppression des itinéraire lorsque l'on change de rayon km

--- public/js/filters.js
+++ public/js/filters.js
@@ -290,7 +290,7 @@ document.getElementById("km").addEventListener('change',function(event) {
             }).on('click', function() {
                this.bindPopup(feature.properties.title+ "<hr>"+feature.properties.date+ "<hr>"+feature.properties.hour+ "<hr>"+feature.properties.price + "<hr>"+feature.properties.address + "<hr>"+"<a href="+feature.properties.url+ "><img width='350px' height='100px' src="+feature.properties.image+"></a>"+ "<hr>" +"<a href="+feature.properties.url+ ">\ud83d\ude33Plus de détails</a>"+"<button type='button' onclick='drawItinary("+userLocation.lat+","+userLocation.lng+","+latlng.lat+","+latlng.lng+")'>Itineraire</button>");
               });
-              // map.removeControl(rControl);
+              map.removeControl(rControl);
               marker._id = feature.properties.id;
               markersTab.push(marker);
               // markersLayer.addLayer(marker);
