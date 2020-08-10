fc00f8878c006dcac2bf9994c68d6ced7d5abda5
Honorine Marie Christy <honorine@24eme.fr>
2020-08-10 12:30:43 +0200

    suppression des commentaires

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.layout')
 @section('content')
 
-<!-- <button type="button" id="others" class="btn btn-secondary btn_searchZone">Recherche dans cette zone</button> -->
 <div id="map"></div>
 <div id='list'><ul class="nav_events" id="ct"></ul></div>
 
