160f9518fdd6a766735858b23b4fe80d027e5fcd
Sidi Sokhona <ssokhona@24eme.fr>
2020-08-10 16:44:22 +0200

    gestion de responsive dans index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 
 <!-- <button type="button" id="others" class="btn btn-secondary btn_searchZone">Recherche dans cette zone</button> -->
-<div id="map"></div>
-<div id='list'><ul class="nav_events" id="ct"></ul></div>
-
+<div class=" col-sm-6 col-xl-8 col-lg-7 col-md-6 m-auto" id="map"></div>
+<ul  class="nav_events col-lg-4 col-sm-6 col-md-4" id="ct"></ul>
 @endsection
